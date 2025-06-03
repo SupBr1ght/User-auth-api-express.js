c8444891c8a9cd22b4a52186d608bdef774ad7ea
sap <pechkursasha@gmail.com>
2025-06-03 13:20:24 +0000

    feat: add user roles into model

--- models/User.js
+++ models/User.js
@@ -4,19 +4,24 @@ import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  roles: {
+    type: String,
+    enum: ["user", "moderator", "admin"],
+    default: "user"
+  },
 });
 
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-userSchema.methods.comparePassword = function(password) {
+userSchema.methods.comparePassword = function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
