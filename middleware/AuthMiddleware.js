233c344552da70d53ae410b7ace88b3a451aae3d
sap <pechkursasha@gmail.com>
2025-06-03 16:33:23 +0000

    feat: implement user's moderation

--- middleware/AuthMiddleware.js
+++ middleware/AuthMiddleware.js
@@ -1,20 +1,19 @@
-import jwt from "jsonwebtoken"
-import User from "../models/User.js"
+import jwt from "jsonwebtoken";
 
 const protect = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader?.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'No token provided' });
+  if (!authHeader?.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token provided" });
   }
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id).select('-password');
+    req.user = decoded;
     next();
   } catch (err) {
-    res.status(401).json({ message: 'Something suspicios tokens not match!' });
+    console.error("JWT error:", err.message);
+    res.status(401).json({ message: "Token verification failed" });
   }
 };
 
-
-export default protect
+export default protect;
--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { register, login } from '../controllers/authController.js';
+import { register, login, updateUserById, deleteUserById } from '../controllers/authController.js';
 import protect from '../middleware/AuthMiddleware.js';
+import permitRoles from "../middleware/RolesVerify.js";
+
 
 const router = express.Router();
 
@@ -9,5 +11,10 @@ router.post('/login', login);
 router.get('/me', protect, (req, res) => {
   res.status(200).json({ user: req.user });
 });
+
+
+// Protected routes
+router.put('/users/:id', protect, permitRoles('admin', 'moderator'), updateUserById);
+router.delete('/users/:id', protect, permitRoles('admin', 'moderator'), deleteUserById);
 
 export default router;
